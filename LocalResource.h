cabd2fa274ec7a54c87d2120f2b83879791de6e4
RoadS1de <wpp147@foxmail.com>
2026-08-26 14:32:40 +0800

    4444

---
+++ LocalResource.h
@@ -0,0 +1,23 @@
+// LocalResource.h: interface for the CLocalResource class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_LOCALRESOURCE_H__5468D25D_5DF8_4367_9341_6209921964A1__INCLUDED_)
+#define AFX_LOCALRESOURCE_H__5468D25D_5DF8_4367_9341_6209921964A1__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+//”Ô—‘
+class CLocalResource  
+{
+public:
+	CLocalResource();
+	virtual ~CLocalResource();
+
+protected:
+	HINSTANCE m_hInstOld;
+};
+
+#endif // !defined(AFX_LOCALRESOURCE_H__5468D25D_5DF8_4367_9341_6209921964A9__INCLUDED_)
